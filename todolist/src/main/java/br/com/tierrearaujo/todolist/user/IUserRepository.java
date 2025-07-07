4e9e9ff40585dd15ba06b17e2b1fe599933a59b6
Seu Nome <tierrearaujo01@gmail.com>
2025-07-07 08:52:42 -0300

    Dockerfile deploy

---
+++ todolist/src/main/java/br/com/tierrearaujo/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.tierrearaujo.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
